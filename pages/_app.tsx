1e6fc72cb130fa457cc4d71c7ed0fa2a47c041d1
dtxrital <deltakbot1@gmail.com>
2023-01-21 11:32:30 +0000

    Add input

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Noto_Sans_KR } from '@next/font/google';
+
+const font = Noto_Sans_KR({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={font.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from 'next';
+import styles from '../styles/index.module.css';
 
 const IndexPage: NextPage = () => {
-  return <h1>main</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>Tlqkf</div>
+      <input className={styles.search}></input>
+    </div>
+  );
 };
 
 export default IndexPage;
